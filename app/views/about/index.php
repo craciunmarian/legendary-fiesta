7d4f702bc410040486fd0cdae3245b3b3c0dad4b
craciunmarian <razvanmariancr@gmail.com>
2021-04-10 19:32:41 +0300

    about index + public/css

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,1 +1,38 @@
-About page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylesheet.css">
+    <title>Document</title>
+</head>
+<header class="header">
+    <a class="header__text" href="pagetest1.html">Acasă</a>
+    <a class="header__text" href="pagetest2.html">Statistici</a>
+    <a class="header__text" href="index.html">Despre noi</a>
+</header>
+<body>
+    <div class="gallery">
+        <div class="portrait">
+            <img class="portrait__picture" src="download.jpg" alt="pic 1" width="150" height="150">
+            <p class="name">Adana Popescu</p>
+            <div class="dash"></div>
+        </div>
+        <div class="portrait">
+            <img class="portrait__picture" src="download.jpg" alt="pic 2" width="150" height="150">
+            <p class="name">Vătui Adrian</p>
+            <div class="dash"></div>
+        </div>
+        <div class="portrait">
+            <img class="portrait__picture" src="download.jpg" alt="pic 3" width="150" height="150">
+            <p class="name">Crăciun Răzvan</p>
+            <div class="dash"></div>
+        </div>
+    </div>
+</body>
+<footer>
+    <p class="footer__text">filler text</p>
+    <p class="footer__text">filler text</p>
+</footer>
+</html>
