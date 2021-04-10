4ca70b74337df3ea7d3cd3bfaf6081f30a372f36
Adana Popescu <adana.popescu22@gmail.com>
2021-04-10 20:55:34 +0300

    landing page v1

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,66 @@
-Landing page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../TW_UnWe_MVC/public/css/home/aa.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    
+</head>
+
+
+<body>
+    <img class= "img1" src="/asset-main.png">
+<header>
+        <div class="header">
+        <div class="text1">
+        Acasă
+        </div>
+        <div class="text1">
+        Statistici
+        </div>
+        <div class="text1">
+        Despre noi
+        </div>   
+        </div>
+    
+</header>
+
+<h1 class="title1 text-center">
+    Șomajul în România
+</h1>
+
+<div class="line">
+</div>
+
+<h2 class="subtitle text-center">
+    funcționalități
+</h2>
+
+<section class="section1 text-center">
+<div class="info">
+<img class="img2" src="/pie-chart.svg">
+<br>
+    Grafice
+    <p class="smalltext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quia architecto veniam rem fugit minima ratione error, debitis molestiae vitae facere, modi adipisci. Incidunt accusamus blanditiis id sequi voluptatum corrupti!</p>
+</div>
+<div class="info">
+    <img class="img2" src="/analytics.svg">
+    <br>
+    Filtrare
+    <p class="smalltext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quia architecto veniam rem fugit minima ratione error, debitis molestiae vitae facere, modi adipisci. Incidunt accusamus blanditiis id sequi voluptatum corrupti!</p>
+</div>
+<div class="info">
+    <img class="img2" src="/document.svg">
+    <br>
+    Export
+    <p class="smalltext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quia architecto veniam rem fugit minima ratione error, debitis molestiae vitae facere, modi adipisci. Incidunt accusamus blanditiis id sequi voluptatum corrupti!</p>
+</div>
+</section>
+
+</body>
+
+</html>
