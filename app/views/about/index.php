048f71bca5a47e5e519c25141f891d4c397fa04e
craciunmarian <razvanmariancr@gmail.com>
2021-04-10 19:43:33 +0300

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./stylesheet.css">
+    <link rel="stylesheet" href="../../../public/css/about/stylesheet.css">
     <title>Document</title>
 </head>
 <header class="header">
@@ -15,17 +15,17 @@
 <body>
     <div class="gallery">
         <div class="portrait">
-            <img class="portrait__picture" src="download.jpg" alt="pic 1" width="150" height="150">
+            <img class="portrait__picture" src="../../../public/assets/about/generic_pic.jpg" alt="pic 1" width="150" height="150">
             <p class="name">Adana Popescu</p>
             <div class="dash"></div>
         </div>
         <div class="portrait">
-            <img class="portrait__picture" src="download.jpg" alt="pic 2" width="150" height="150">
+            <img class="portrait__picture" src="../../../public/assets/about/generic_pic.jpg" alt="pic 2" width="150" height="150">
             <p class="name">Vătui Adrian</p>
             <div class="dash"></div>
         </div>
         <div class="portrait">
-            <img class="portrait__picture" src="download.jpg" alt="pic 3" width="150" height="150">
+            <img class="portrait__picture" src="../../../public/assets/about/generic_pic.jpg" alt="pic 3" width="150" height="150">
             <p class="name">Crăciun Răzvan</p>
             <div class="dash"></div>
         </div>
