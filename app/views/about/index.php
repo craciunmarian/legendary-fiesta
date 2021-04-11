7eddfb2f5bfa9947e398b38db94d6f184e4df8d6
craciunmarian <razvanmariancr@gmail.com>
2021-04-11 13:54:22 +0300

    about page change

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../../TW_UnWe_MVC/public/css/about/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <title>UnWe - Unemployment Web About</title>
 </head>
 <body>
@@ -15,7 +17,7 @@
     </header>
     <div class="gallery">
         <div class="portrait">
-            <img class="portrait__picture" src="../../../../TW_UnWe_MVC/public/assets/about/adana_pfp.jpg" alt="pic 1">
+            <img class="portrait__picture" src="../../../../TW_UnWe_MVC/public/assets/about/adana_pfp.png" alt="pic 1">
             <p class="name">Adana Popescu</p>
             <div class="dash"></div>
             <p class="email">idkyet@example.com</p>
