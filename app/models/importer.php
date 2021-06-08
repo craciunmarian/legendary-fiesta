965fc337eefab191a703ab5e51dfe5ce5763fee9
Adana Popescu <adana.popescu22@gmail.com>
2021-06-09 02:27:22 +0300

    started working on adding entries to database

--- app/models/importer.php
+++ app/models/importer.php
@@ -7,6 +7,7 @@ class Importer
     private const start = 'https://data.gov.ro/dataset?q=somaj&sort=metadata_modified+desc&res_format=.csv';
     private const gov = 'https://data.gov.ro';
     private $judete = [];
+    private $addToDB = array();
 
     public function __construct($datasource)
     {
@@ -27,7 +28,13 @@ class Importer
 
         foreach ($nodeList as $node) {
             $append = $node->getAttribute("href");
+            $this->format_date($append);
             $this->follow_CSV_links(self::gov . $append);
+
+            var_dump($this->addToDB);
+            echo "<br>";
+            $this->addToDB = array();
+            die();
         }
     }
 
@@ -58,6 +65,8 @@ class Importer
 
         $nodeList = $xpath->query("//div[@class='archiver link-cached']/a");
 
+        $i = 0;
+
         foreach ($nodeList as $node) {
             $file =  fopen($node->getAttribute("href"), "r");
             $line = fgetcsv($file, 0, ",",);
@@ -69,8 +78,9 @@ class Importer
                 $name = $next[0];
                 $name = $this->format_city_names($name);
 
-                array_push($this->judete, $name);
-
+                //array_push($this->judete, $name);
+                $this->addToDB[$i] = array_merge($this->addToDB[$i], $next);
+                $i++;
 
                 //var_dump($next);
                 //echo ($next[0]);
@@ -109,6 +119,27 @@ class Importer
         return $name;
     }
 
+    public function format_date($whole)
+    {
+        $luni = array("ianuarie", "februarie", "martie", "aprilie", "mai", "iunie", "iulie", "august", "septembrie", "octombrie", "noiembrie", "decembrie");
+        $replace_luni = array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+
+        $splitLink = explode("-", $whole);
+        $month = $splitLink[count($splitLink) - 2];
+        $year = $splitLink[count($splitLink) - 1];
+
+        $month = str_replace($luni, $replace_luni, $month);
+        $date = $year . '-' . $month . '-' . '01';
+
+        if ($date == 'mai2020-inregistrat-01')
+            $date = '2020-05-01';
+        if ($date == '2019-sepembrie-01')
+            $date = '2019-09-01';
+
+        for ($i = 0; $i < 43; $i++)
+            $this->addToDB[$i][0] = $date;
+    }
+
 
 
     // public function crawl()
@@ -119,7 +150,7 @@ class Importer
     {
         $this->follow_main_links(self::start);
         $this->judete = array_unique($this->judete);
-        var_dump($this->judete);
+        //var_dump($this->judete);
         // web crawling etc.
         // $this->db->insertRow(array(
         //     '2012-08-06', 'judet1', 1, 4, 5, 1.12, 1.13, 1.14,
