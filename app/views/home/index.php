d9548b4f545f87ba787fcca1be97b65d936061da
Adrian Vatui <adrianvatui9989@yahoo.com>
2021-06-10 05:58:06 +0300

    meta description

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UnWe - Unemployment Web Visualizer</title>
     <link rel="stylesheet" href="/css/common/common.css">
     <link rel="stylesheet" href="/css/home/landing_page.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-
+    <title>UnWe - Unemployment Web Visualizer</title>
+    <meta name="description" content="Generate charts with official unemployment data in Romania">
 </head>
 
 
